8b9bba7f1a8631032cff67e40a65146f6556ac1c
oznerol <>
2022-06-20 16:40:24 +0800

    windows changes: resolved conflict.

--- main.go
+++ main.go
@@ -8,4 +8,15 @@ func main() {
 	fmt.Println("Git Demo")
 
 	fmt.Println("Resolving git merge while rebase")
+
+	ch := make(chan string, 3)
+
+	ch <- "Veda"
+	ch <- "Omega"
+	ch <- "Heyjay"
+	close(ch)
+
+	for s := range ch {
+		fmt.Println("Employee Name: ", s)
+	}
 }
